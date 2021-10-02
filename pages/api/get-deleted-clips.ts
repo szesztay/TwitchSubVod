154006bca20b99b9478ae5e5ec8b58b678a80544
Alisson Leal <alissonsleal@hotmail.com>
2021-10-02 12:56:53 -0300

    feat: save deleted clips links in database

--- pages/api/get-deleted-clips.ts
+++ pages/api/get-deleted-clips.ts
@@ -1,20 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 import connectDB from '@/utils/backend/middleware/mongodb';
-import MostWatched from '@/utils/backend/models/mostWatched';
-import MostWatchedStreamer from '@/utils/backend/models/mostWatchedStreamers';
+import DeletedClips from '@/utils/backend/models/deletedClips';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  // test vod: 43911503933
   switch (req.method) {
     case 'GET':
       const { vod, start, end } = req.query;
-      const vodId = 43911503933;
       let urls: Promise<string | void>[] = [];
 
       if (vod && start && end) {
         for (let i = Number(start); i <= Number(end); i++) {
           // console.log(loopNumber);
-          let url = `https://clips-media-assets2.twitch.tv/${vodId}-offset-${i}.mp4`;
+          let url = `https://clips-media-assets2.twitch.tv/${vod}-offset-${i}.mp4`;
           const data = axios
             .head(url)
             .then(() => {
@@ -29,6 +28,26 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
         const result = await Promise.all(urls);
         const filteredResult = result.filter(Boolean);
+
+        if (filteredResult.length > 0) {
+          try {
+            const insertedClips = await DeletedClips.findOneAndUpdate(
+              { vod },
+              {
+                vod: vod,
+                $addToSet: {
+                  clips: filteredResult,
+                  searchedZones: [{ start: Number(start), end: Number(end) }],
+                },
+              },
+              { upsert: true, new: true },
+            );
+
+            console.log(insertedClips);
+          } catch (err) {
+            console.log(err.message);
+          }
+        }
 
         try {
           return res.status(200).json({ success: true, clips: filteredResult });
