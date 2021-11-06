788267b492c1320323efc88d69a8ec0801be196c
Alisson Leal <alissonsleal@hotmail.com>
2021-11-06 12:46:40 -0300

    feat: client side load balancing with new EU servers

--- utils/services/cors.ts
+++ utils/services/cors.ts
@@ -1,9 +1,16 @@
 export const cors = () => {
+  const corsEnv = process.env.NEXT_PUBLIC_CORS?.split(',') || [];
+  const corsEnvEU = process.env.NEXT_PUBLIC_CORS_EU?.split(',') || [];
+
   const value = Math.random();
 
-  if (value >= 0.5) {
-    return process.env.NEXT_PUBLIC_CORS;
+  const isInEurope = Intl.DateTimeFormat()
+    .resolvedOptions()
+    .timeZone.includes('Europe');
+
+  if (isInEurope) {
+    return corsEnvEU[Math.floor(value * corsEnvEU.length)];
   }
 
-  return process.env.NEXT_PUBLIC_CORS_2;
+  return corsEnv[Math.floor(value * corsEnv.length)];
 };
