46722b8ea3ec9e89999fe9578384cb914da92024
Alisson Leal <alissonsleal@hotmail.com>
2021-12-19 16:12:57 -0300

    test create ButtonPill

---
+++ src/components/atoms/ButtonPill/index.spec.tsx
@@ -0,0 +1,60 @@
+import { render, screen } from '@testing-library/react'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme } from '~/layout/theme'
+import Icon from '.'
+import { FaTwitch } from 'react-icons/fa'
+import ButtonPill from '.'
+import { RiFeedbackFill } from 'react-icons/ri'
+
+describe('ButtonPill component', () => {
+  it('should render correctly', () => {
+    const fn = jest.fn()
+
+    render(
+      <ThemeProvider theme={darkTheme}>
+        <ButtonPill text="Feedback" onClick={fn} />
+      </ThemeProvider>,
+    )
+
+    const icon = screen.getByRole('button', { name: 'Feedback' })
+    expect(icon).toBeInTheDocument()
+  })
+
+  it('should render correctly with icon on left', () => {
+    const fn = jest.fn()
+
+    render(
+      <ThemeProvider theme={darkTheme}>
+        <ButtonPill
+          icon={<RiFeedbackFill />}
+          iconPosition="left"
+          text="Feedback"
+          title="Send feedback"
+          onClick={fn}
+        />
+      </ThemeProvider>,
+    )
+
+    const icon = screen.getByRole('button', { name: 'Send feedback' })
+    expect(icon).toBeInTheDocument()
+  })
+
+  it('should render correctly with icon on right', () => {
+    const fn = jest.fn()
+
+    render(
+      <ThemeProvider theme={darkTheme}>
+        <ButtonPill
+          icon={<RiFeedbackFill />}
+          iconPosition="right"
+          text="Feedback"
+          title="Send feedback"
+          onClick={fn}
+        />
+      </ThemeProvider>,
+    )
+
+    const icon = screen.getByRole('button', { name: 'Send feedback' })
+    expect(icon).toBeInTheDocument()
+  })
+})
