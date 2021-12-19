1761c57cdb6b8724117208833838582082507f4a
Alisson Leal <alissonsleal@hotmail.com>
2021-12-18 21:18:48 -0300

    feat: create and use absolute import with aliases

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import type { AppProps } from 'next/app'
-import BasicLayout, { darkTheme, lightTheme } from '../layout/Basic'
+import BasicLayout, { darkTheme, lightTheme } from '~/layout/Basic'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [theme, setTheme] = useState('dark')
