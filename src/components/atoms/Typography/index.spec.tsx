6454fade5b0ea00d6f98925c77607c6f4925f26b
Alisson Leal <alissonsleal@hotmail.com>
2021-12-19 17:36:38 -0300

    test: create typography component

---
+++ src/components/atoms/Typography/index.spec.tsx
@@ -0,0 +1,65 @@
+import { render, screen } from '@testing-library/react'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme } from '~/layout/theme'
+import Typography from '.'
+import { typographyVariants } from './constants'
+import 'jest-styled-components'
+
+describe('Typography component', () => {
+  it('should render correctly', () => {
+    render(
+      <ThemeProvider theme={darkTheme}>
+        {Object.keys(typographyVariants).map((variant) => (
+          <Typography
+            key={variant}
+            variant={variant as keyof typeof typographyVariants}
+          >
+            {variant}
+          </Typography>
+        ))}
+      </ThemeProvider>,
+    )
+
+    Object.keys(typographyVariants).forEach((variant) => {
+      const Typography = screen.getByText(variant)
+      expect(Typography).toHaveStyleRule(
+        'font-size',
+        typographyVariants[variant as keyof typeof typographyVariants].fontSize,
+      )
+      expect(Typography).toHaveStyleRule(
+        'font-weight',
+        typographyVariants[variant as keyof typeof typographyVariants]
+          .fontWeight,
+      )
+      expect(Typography).toHaveStyleRule(
+        'line-height',
+        typographyVariants[variant as keyof typeof typographyVariants]
+          .lineHeight,
+      )
+
+      expect(Typography).toBeInTheDocument()
+    })
+  })
+
+  it('should render correctly with color', () => {
+    render(
+      <ThemeProvider theme={darkTheme}>
+        {Object.keys(typographyVariants).map((variant) => (
+          <Typography
+            key={variant}
+            variant={variant as keyof typeof typographyVariants}
+            color="#ff6699"
+          >
+            {variant}
+          </Typography>
+        ))}
+      </ThemeProvider>,
+    )
+
+    Object.keys(typographyVariants).forEach((variant) => {
+      const Typography = screen.getByText(variant)
+      expect(Typography).toHaveStyleRule('color', '#ff6699')
+      expect(Typography).toBeInTheDocument()
+    })
+  })
+})
